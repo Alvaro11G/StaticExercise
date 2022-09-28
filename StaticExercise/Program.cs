22b79e71356c5adee484493cd2fc2a989818dc0e
Alvaro11G <agarcia11746@gmail.com>
2022-09-28 11:50:00 -0600

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = TempConverter.FahrenheitToCelsius(68);
+            double fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"Celsius: {celsius} degrees");
+            Console.WriteLine($"Fahrenheit: {fahrenheit} degrees");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+         public static double FahrenheitToCelsius(double f)
+        {
+            double Celsius = (f - 32) * 5 / 9;
+
+            return Celsius;
+        }
+        public static double CelsiusToFahrenheit(double c)
+        {
+            double Fahrenheit = (c * 9) / 5 + 32;
+
+            return Fahrenheit;
+        }
+    }
+}
